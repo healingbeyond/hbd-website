d6cdaabeb62eeda58c4fac1d1f66ca973127b984
healingbeyond <corey@healingbeyonddiagnosis.ca>
2026-09-07 17:27:30 -0400

    Route public BDRS overview

--- src/auth/public/PublicAppRoutes.tsx
+++ src/auth/public/PublicAppRoutes.tsx
@@ -5,6 +5,7 @@ import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { HomePage } from "@/pages/HomePage";
 import { AboutPage } from "@/pages/AboutPage";
+import { BdrsPage } from "@/pages/BdrsPage";
 import { ToolPage } from "@/pages/ToolPage";
 import { PodcastPage } from "@/pages/PodcastPage";
 import { ResourcesPage } from "@/pages/ResourcesPage";
@@ -40,6 +41,7 @@ export function PublicAppRoutes() {
         <Route element={<PublicShell />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/bdrs" element={<BdrsPage />} />
           <Route path="/assessments" element={<Navigate to="/resources" replace />} />
           <Route path="/assessments/:toolId" element={<ToolPage />} />
           <Route path="/podcast" element={<PodcastPage />} />
